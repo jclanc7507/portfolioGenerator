895d633639d89f83406550fa3ad84b7b3a0871c4
Jeremiah <jclanc7507@gmail.com>
2022-02-21 12:28:49 -0500

    generates html file when node is executed

--- app.js
+++ app.js
@@ -1,12 +1,30 @@
-var profileDataArgs = process.argv.slice(
-    2, process.argv.length
-    );
-console.log(profileDataArgs);
+const profileDataArgs = process.argv.slice(2, process.argv.length);
+const [name, github] = profileDataArgs; 
 
-console.log('================');
+const fs = require('fs');
 
-const printProfileData = profileDataArr => {
-    profileDataArr.forEach((profileItem) => console.log(profileItem));
+const generatePage = (userName, github) => {
+
+    return `
+    <!DOCTYPE html>
+    <html lang="en">
+    <head>
+        <meta charset="UTF-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title>Portfolio Demo</title>
+    </head>
+
+    <body>
+        <h1>${name}</h1>
+        <h2><a href="https://github.com/${github}">Github Page</a></h2>
+    </body>
+    </html>
+    `;
 };
 
-printProfileData(profileDataArgs);
+fs.writeFile('index.html', generatePage(name, github), err => {
+    if (err) throw err;
+
+    console.log('Portfolio complete! Check index.html for output.')
+});
