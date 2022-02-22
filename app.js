57cb1b2e3d35f70cf412bc96c43625ccd2f1c324
Jeremiah B Clancy <94941750+jclanc7507@users.noreply.github.com>
2022-02-22 09:15:55 -0500

    Adding User Prompts

--- app.js
+++ app.js
@@ -1,7 +1,27 @@
 const fs = require('fs');
 const generatePage = require('./src/page-template.js');
-const profileDataArgs = process.argv.slice(2, process.argv.length);
-const [name, github] = profileDataArgs; 
+var inquirer = require('inquirer');
+
+const promptUser = () => {
+return inquirer.prompt([ 
+{
+type: 'input', 
+name: 'name', 
+message: 'What is your name?'
+}, 
+{
+type: 'input', 
+name: 'github', 
+message: 'Enter your GitHub Username'
+}, 
+{
+type: 'input', 
+name: 'about', 
+message: 'Provide some information about yourself:' 
+}, ]) 
+};
+
+promptUser.then(answers => console.log(answers));
 
 fs.writeFile('index.html', generatePage(name, github), err => {
     if (err) throw err;
