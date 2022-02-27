54ddb44a56c5d87b6c68402f2ff7bec75e127f48
Jeremiah <jclanc7507@gmail.com>
2022-02-27 15:04:10 -0500

    finalized refactoring Promises

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
-const fs = require('fs');
+const { writeFile, copyFile } = require('./utils/generate-site.js');
 const inquirer = require('inquirer');
 const generatePage = require('./src/page-template');
 
@@ -131,12 +131,18 @@ Add a New Project
 promptUser()
 .then(promptProject)
 .then(portfolioData => {
-    console.log(portfolioData);
-    const pageHTML = generatePage(portfolioData);
-        
-    fs.writeFile('./dist/index.html', pageHTML, err => {
-        if (err) throw new Error(err);
-
-        console.log('Portfolio complete! Check index.html for output.');
-    });
+    return generatePage(portfolioData);
+})
+.then(pageHTML => {
+    return writeFile(pageHTML);
+})
+.then(writeFileResponse => {
+    console.log(writeFileResponse);
+    return copyFile();
+})
+.then(copyFileResponse => {
+    console.log(copyFileResponse);
+})
+.catch(err => {
+    console.log(err);
 });
